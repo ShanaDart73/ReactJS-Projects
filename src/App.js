1c9bc44b734d86ddca7451b0923755ea6f66dd79
ShanaDart73 <carole.kayungu@gmail.com>
2023-11-30 11:06:33 +0100

    began homepage layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import './sass/home_style.css';
 
 function App() {
   return (
-      <div className="body">
-          <h1>Hello React1</h1>
+      <div className="container">
+          <header>
+              <h1>React.JS</h1>
+          </header>
+
+          <div className="content">
+              <section>
+                  <div>Prime Number</div>
+              </section>
+              <section>
+                  <div>Factorial</div>
+              </section>
+              <section>
+                  <div>Random Number</div>
+              </section>
+              <section>
+                  <div>ASCII Value</div>
+              </section>
+          </div>
+
+          <footer>
+              <p>React.JS Projects</p>
+          </footer>
       </div>
+
   );
 }
 
