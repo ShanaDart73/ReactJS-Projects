ecc73e06d237aad8e303b66d0720ceaf98e7c21b
ShanaDart73 <carole.kayungu@gmail.com>
2023-12-04 21:25:38 +0100

    completed header and content layout

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import './sass/home_style.css';
 function App() {
   return (
       <div className="container">
-          <header>
-              <div className="hero">
-                  <h1>React.JS</h1>
-              </div>
-          </header>
-
           <div className="content">
+              <header>
+                  <div className="hero">
+                      <h1>React.JS</h1>
+                  </div>
+              </header>
               <section className="one">
-                  <div>Prime Number</div>
+                  <div className="text"><h2>Prime Number</h2></div>
               </section>
               <section className="two">
-                  <div>Factorial</div>
+                  <div className="text"><h2>Factorial</h2></div>
               </section>
               <section className="three">
-                  <div>Random Number</div>
+                  <div className="text"><h2>Random Number</h2></div>
               </section>
               <section className="four">
-                  <div>ASCII Value</div>
+                  <div className="text"><h2>ASCII Value</h2></div>
               </section>
+              <footer>
+                  <p>React.JS Projects</p>
+              </footer>
           </div>
 
-          <footer>
-              <p>React.JS Projects</p>
-          </footer>
+
       </div>
 
   );
